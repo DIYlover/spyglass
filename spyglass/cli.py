6587f39448efb8f50d06c894b9806881de2e6dc2
DIYLover <willy.w650@gmail.com>
2024-07-25 18:26:07 +0800

    Change the description of new parameter for -n

--- spyglass/cli.py
+++ spyglass/cli.py
@@ -142,7 +142,7 @@ def get_parser():
         prog='spyglass',
         description='Start a webserver for Picamera2 videostreams.',
         formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument('-n', '--csi_port_no', type=int, default=0, help='Bind to CSI port number, default is 0, additional port in Pi5 is 1')
+    parser.add_argument('-n', '--csi_port_no', type=int, default=0, help='Bind to CSI port number, additional port in Raspberry Pi 5 is 1, default is 0 if not specify')
     parser.add_argument('-b', '--bindaddress', type=str, default='0.0.0.0', help='Bind to address for incoming '
                                                                                  'connections')
     parser.add_argument('-p', '--port', type=int, default=8080, help='Bind to port for incoming connections')
